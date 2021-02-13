dac0a0abcbd7335be909a543cc97ed2b85d88bb7
kastei <glomukhin@gmail.com>
2021-02-12 22:58:55 -0800

    new folder

--- course 1/2.1/2.4.1.py
+++ course 1/2.1/2.4.1.py
@@ -1,6 +1,6 @@
 
-in_file = '../tmp/dataset_24465_4.txt'
-out_file = '../tmp/out.txt'
+in_file = '../../tmp/dataset_24465_4.txt'
+out_file = '../../tmp/out.txt'
 with open(in_file, 'r') as in_f, open(out_file, 'w') as out_f:
     lines = in_f.readlines()
     lines.reverse()
